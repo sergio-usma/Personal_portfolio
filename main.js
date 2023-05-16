02547328750264ad33ed7cfe5b4ebc97ee7f1ac0
sergio-usma <sergioandresusma@hotmail.com>
2023-05-16 15:53:36 -0500

    Added EsLint linter for JS

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
 // Dom  items
-const header =  document.querySelector('#header');
-const burger =  document.querySelector('#burger');
-const body=document.querySelector("body")
+const header = document.querySelector('#header');
+const burger = document.querySelector('#burger');
 
-let  showMenu = false;
- burger.addEventListener('click', toggleMenu);
+let showMenu = false;
 
- function toggleMenu() {
-    if(!showMenu) {
-        header.classList.remove('header');
-        header.classList.add('header-active');
-        burger.classList.remove('burger');
-        burger.classList.add('burger-active');
-        document.body.style.overflowY = "hidden";
+function toggleMenu() {
+  if (!showMenu) {
+    header.classList.remove('header');
+    header.classList.add('header-active');
+    burger.classList.remove('burger');
+    burger.classList.add('burger-active');
+    document.body.style.overflowY = 'hidden';
 
-        // set  menu state
-        showMenu = true;
-    } else {
-        header.classList.remove('header-active');
-        header.classList.add('header');
-        burger.classList.remove('burger-active');
-        burger.classList.add('burger');
-        document.body.style.overflowY = "auto";
+    // set  menu state
+    showMenu = true;
+  } else {
+    header.classList.remove('header-active');
+    header.classList.add('header');
+    burger.classList.remove('burger-active');
+    burger.classList.add('burger');
+    document.body.style.overflowY = 'auto';
 
-
-        //Set menu state
-        showMenu = false;
-     
-    }
-
-
- }
+    // Set menu state
+    showMenu = false;
+  }
+}
+burger.addEventListener('click', toggleMenu);
